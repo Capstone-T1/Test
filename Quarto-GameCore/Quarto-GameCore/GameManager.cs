16320382f6bcb1c65a46befdfbe4c6b395767808
tdurey <tdurey1@harding.edu>
2020-01-31 16:21:07 -0600

    Added Game-Core for funsies

---
+++ Quarto-GameCore/Quarto-GameCore/GameManager.cs
@@ -0,0 +1,34 @@
+﻿namespace Quarto_GameCore
+{
+    class GameManager
+    {
+        static void Main(string[] args)
+        {
+            GameBoard gameBoard = new GameBoard();
+            bool exitGame = false;
+            gameBoard.drawBoard();
+
+            while (exitGame == false)
+            {
+                gameBoard.piecesRemaining();
+
+                if (!gameBoard.placeGamePiece())
+                    continue;
+
+                if (gameBoard.isGameOver())
+                {
+                    exitGame = gameBoard.startNewGame();
+
+                    if(exitGame)
+                    {
+                        gameBoard = new GameBoard();
+                        gameBoard.drawBoard();
+                        exitGame = false;
+                    }
+                }
+                else
+                    gameBoard.drawBoard(); 
+            }
+        }
+    }
+}
